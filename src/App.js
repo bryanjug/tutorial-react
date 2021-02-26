33969de9bc3aeef9622df579e2f19d4a19822398
bryanjug <bryandjug@gmail.com>
2021-02-25 19:49:17 -0800

    tutorial from youtube finished

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import Tweet from './Tweet';
+
+function App() {
+
+  const [users, setUsers] = useState ([
+    {name: 'Ed', message: 'Hello there'},
+    {name: 'John', message: 'I am john snow'},
+    {name: "travery", message: 'I am awesome'}
+  ]);
+
+  return (
+    <div className='app'>
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
+    </div>
+  );
+}
+
+export default App;
